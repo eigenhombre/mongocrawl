477b6bc8bded4520e6d4dfa608ce44c6cb515133
John Jacobsen <john@mail.npxdesigns.com>
2012-05-15 16:02:33 -0500

    Initial lein setup

---
+++ src/mongocrawl/core.clj
@@ -0,0 +1,1 @@
+(ns mongocrawl.core)
---
+++ test/mongocrawl/test/core.clj
@@ -0,0 +1,6 @@
+(ns mongocrawl.test.core
+  (:use [mongocrawl.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
