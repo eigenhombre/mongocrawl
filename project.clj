c0cde88925eff0ac219e760bd387481b9b7992e1
Matthew Rocklin <mrocklin@gmail.com>
2012-05-16 07:42:27 -0500

    added dependencies
    Github api
    https://github.com/Raynes/tentacles
    
    MongoDB api
    http://clojuremongodb.info/articles/getting_started.html#monger_overview
    
    autoexpect

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
 (defproject mongocrawl "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[tentacles "0.1.3"]
+                 [com.novemberain/monger "1.0.0-beta6"]
+                 [org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[lein-expectations "0.0.1"]
+                     [lein-autoexpect "0.1.1"]
+                     [expectations "1.3.6"]])
