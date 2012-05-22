5fc28361d8f6c06682aeb51208992b9194ee2fc0
John Jacobsen <john@mail.npxdesigns.com>
2012-05-22 09:03:33 -0500

    Another implementation using atoms

--- src/mongocrawl/core.clj
+++ src/mongocrawl/core.clj
@@ -15,16 +15,15 @@
 
 (def clojure (repos/specific-repo "clojure" "clojure"))
 
-(def visited-people (ref #{}))
-(def visited-repos (ref #{}))
-
 (def clojure-collabs
   (map :login (repos/collaborators "clojure" "clojure")))
 
 (defn repos-of-user [u]
+  (print "R") (flush)
   (map :name (repos/user-repos u)))
 
 (defn users-of-repo [u r]
+  (print "u") (flush)
   (map :login (repos/collaborators u r)))
 
 (defn update-users [users repos]
@@ -40,7 +39,7 @@
         (rekey :a [1 2 3]))
 
 (defn users-to-userrepos [users]
-  (apply concat (map #(rekey % (get users %)) 
+  (apply concat (map #(rekey % (get users %))
        (keys users))))
 (expect [[:a 1] [:a 2] [:a 3] [:b 3] [:b 4] [:b 5]]
         (users-to-userrepos {:a [1 2 3] :b [3 4 5]}   ))
@@ -52,11 +51,45 @@
         new-repos (zipmap new-user-repos new-names)]
     (merge repos new-repos)))
 
-        
-(defn step 
+(defn step
   ([users repos]
   [(update-users users repos) (update-repos users repos)])
   ([[users repos]] (step users repos)))
 
-(pp/pprint (step (step {"sympy" ["sympy"]} 
-                       {})))
+;(pp/pprint (step (step {"sympy" ["sympy"]}
+;                       {})))
+
+;;; Another whack
+
+(def users (atom {}))
+(def repos (atom {}))
+
+(defn expand-users
+  ([username]
+     (if-not (some #{username} @users)
+       (let [repos (repos-of-user username)]
+         (swap! users assoc username repos))))
+  ([]
+     (doseq [repo (keys @repos)
+             user (@repos repo)]
+       (expand-users user))))
+
+(defn expand-repos
+  ([username project]
+     (if-not (some #{[username project]} @repos)
+       (doseq [user (users-of-repo username project)]
+         (swap! repos update-in [[username project]] conj user))))
+  ([]
+     (doseq [user (keys @users)
+             repo (@users user)]
+       (expand-repos user repo))))
+
+(defn crawl-github [start-user start-repo]
+  (expand-repos start-user start-repo)
+  (doseq [i (range 2)]
+    (expand-users)
+    (expand-repos)
+    (println (format "Iteration %d: %d users, %d repos"
+                     i (count @users) (count @repos)))))
+
+(crawl-github "clojure" "clojure")
