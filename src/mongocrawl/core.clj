2a775abd4b3d73a15d69c941e6babd728fadfe57
Matthew Rocklin <mrocklin@gmail.com>
2012-05-21 22:56:24 -0500

    Can step outwards. A bit ugly.

--- src/mongocrawl/core.clj
+++ src/mongocrawl/core.clj
@@ -27,9 +27,6 @@
 (defn users-of-repo [u r]
   (map :login (repos/collaborators u r)))
 
-(defn step [users repos]
-  )
-
 (defn update-users [users repos]
   (let [all-users (set (apply concat (vals repos)))
         new-names (remove users all-users)
@@ -37,11 +34,29 @@
         new-users (zipmap new-names new-repos)]
     (merge users new-users)))
 
+(defn rekey [user repos]
+  (for [repo repos] [user repo]))
+(expect [[:a 1] [:a 2] [:a 3]]
+        (rekey :a [1 2 3]))
+
+(defn users-to-userrepos [users]
+  (apply concat (map #(rekey % (get users %)) 
+       (keys users))))
+(expect [[:a 1] [:a 2] [:a 3] [:b 3] [:b 4] [:b 5]]
+        (users-to-userrepos {:a [1 2 3] :b [3 4 5]}   ))
+
 (defn update-repos [users repos]
-  (let [all-repos 1];(set (map #([(apply concat (vals users)))]
-    ))
+  (let [all-user-repos (users-to-userrepos users)
+        new-user-repos (remove repos all-user-repos)
+        new-names (map (fn [[x y]] (users-of-repo x y)) new-user-repos)
+        new-repos (zipmap new-user-repos new-names)]
+    (merge repos new-repos)))
 
-(println (update-users {"mrocklin" ["sympy"]}
-                       {["seed" "seed"]
-                        ["mrocklin" "eigenhombre"]}))
+        
+(defn step 
+  ([users repos]
+  [(update-users users repos) (update-repos users repos)])
+  ([[users repos]] (step users repos)))
 
+(pp/pprint (step (step {"sympy" ["sympy"]} 
+                       {})))
